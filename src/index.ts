f383606a77be71dc99596458be0102d99a40391e
Robert Soriano <sorianorobertc@gmail.com>
2022-03-04 15:11:28 -0800

    update bundler

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,42 @@
 export default function createStack<T>(current: T) {
-	const stack = [current];
+  const stack = [current];
 
-	let index = stack.length;
+  let index = stack.length;
 
   // @ts-ignore
-	let state = {
-		first: true,
-		last: true,
-		current
-	};
+  let state = {
+    first: true,
+    last: true,
+    current,
+  };
 
-	function update() {
-		current = stack[index - 1];
+  function update() {
+    current = stack[index - 1];
 
-		state = {
-			first: index === 1,
-			last: index === stack.length,
-			current
-		};
+    state = {
+      first: index === 1,
+      last: index === stack.length,
+      current,
+    };
 
-		return current;
-	}
+    return current;
+  }
 
-	return {
-		push: (value: T | ((payload: T) => T)) => {
-			stack.length = index;
-			// @ts-ignore
-			stack[index++] = typeof value === 'function' ? (value)(current) : value;
+  return {
+    push: (value: T | ((payload: T) => T)) => {
+      stack.length = index;
+      // @ts-ignore
+      stack[index++] = typeof value === "function" ? value(current) : value;
 
-			return update();
-		},
-		undo: () => {
-			if (index > 1) index -= 1;
-			return update();
-		},
-		redo: () => {
-			if (index < stack.length) index += 1;
-			return update();
-		}
-	};
+      return update();
+    },
+    undo: () => {
+      if (index > 1) index -= 1;
+      return update();
+    },
+    redo: () => {
+      if (index < stack.length) index += 1;
+      return update();
+    },
+  };
 }
---
+++ test/index.test.ts
@@ -0,0 +1,38 @@
+import { test, expect } from "vitest";
+import createStack from "../src";
+
+test("returns the first/last value if stack is in initial state", () => {
+  const value = { x: 1 };
+  const stack = createStack(value);
+
+  expect(stack.undo()).toMatchObject(value);
+  expect(stack.redo()).toMatchObject(value);
+});
+
+test("goes back and forward through history state", () => {
+  const stack = createStack({ x: 1 });
+  stack.push({ x: 2 });
+  stack.push({ x: 3 });
+  stack.push((value) => ({ x: value.x + 1 }));
+
+  expect(stack.undo()).toMatchObject({ x: 3 });
+  expect(stack.undo()).toMatchObject({ x: 2 });
+  expect(stack.undo()).toMatchObject({ x: 1 });
+  expect(stack.redo()).toMatchObject({ x: 2 });
+  expect(stack.redo()).toMatchObject({ x: 3 });
+  expect(stack.redo()).toMatchObject({ x: 4 });
+});
+
+test("clears later values when pushing", () => {
+  const stack = createStack({ x: 1 });
+  stack.push({ x: 2 });
+  stack.push({ x: 3 });
+
+  expect(stack.undo()).toMatchObject({ x: 2 });
+  stack.push({ x: 4 });
+
+  expect(stack.undo()).toMatchObject({ x: 2 });
+  expect(stack.undo()).toMatchObject({ x: 1 });
+  expect(stack.redo()).toMatchObject({ x: 2 });
+  expect(stack.redo()).toMatchObject({ x: 4 });
+});
