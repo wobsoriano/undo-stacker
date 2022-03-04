dbb764e8796b9821cfa85750b03e0e5048ecd51c
Robert Soriano <sorianorobertc@gmail.com>
2022-03-04 15:53:43 -0800

    add linter

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,29 @@
 export default function createStack<T>(current: T) {
-  const stack = [current];
+  const stack = [current]
 
-  let index = stack.length;
-
-  // @ts-ignore
-  let state = {
-    first: true,
-    last: true,
-    current,
-  };
+  let index = stack.length
 
   function update() {
-    current = stack[index - 1];
+    current = stack[index - 1]
 
-    state = {
-      first: index === 1,
-      last: index === stack.length,
-      current,
-    };
-
-    return current;
+    return current
   }
 
   return {
     push: (value: T | ((payload: T) => T)) => {
-      stack.length = index;
-      // @ts-ignore
-      stack[index++] = typeof value === "function" ? value(current) : value;
+      stack.length = index
+      // @ts-expect-error: Value can be a function
+      stack[index++] = typeof value === 'function' ? value(current) : value
 
-      return update();
+      return update()
     },
     undo: () => {
-      if (index > 1) index -= 1;
-      return update();
+      if (index > 1) index -= 1
+      return update()
     },
     redo: () => {
-      if (index < stack.length) index += 1;
-      return update();
+      if (index < stack.length) index += 1
+      return update()
     },
-  };
+  }
 }
