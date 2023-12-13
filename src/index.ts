29846569bf45985329b5b0ad585d1553f509147d
wobsoriano <sorianorobertc@gmail.com>
2023-12-13 09:25:34 -0800

    style: switch to biome

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,31 @@
 export default function createStack<T>(current: T) {
-  const stack = [current]
+	const stack = [current];
 
-  let index = stack.length
+	let index = stack.length;
 
-  function update() {
-    current = stack[index - 1]
+	function update() {
+		// biome-ignore lint/style/noParameterAssign: This is a valid use case for parameter reassignment
+		current = stack[index - 1];
+		return current;
+	}
 
-    return current
-  }
+	return {
+		push: (value: T | ((current: T) => T)) => {
+			stack.length = index;
+			stack[index++] =
+				typeof value === 'function'
+					? (value as (current: T) => T)(current)
+					: value;
 
-  return {
-    push: (value: T | ((current: T) => T)) => {
-      stack.length = index
-      stack[index++] = typeof value === 'function' ? (value as (current: T) => T)(current) : value
-
-      return update()
-    },
-    undo: () => {
-      if (index > 1)
-        index -= 1
-      return update()
-    },
-    redo: () => {
-      if (index < stack.length)
-        index += 1
-      return update()
-    },
-  }
+			return update();
+		},
+		undo: () => {
+			if (index > 1) index -= 1;
+			return update();
+		},
+		redo: () => {
+			if (index < stack.length) index += 1;
+			return update();
+		},
+	};
 }
